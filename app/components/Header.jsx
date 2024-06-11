8f40eccad21609e32f8a5b5f33304f3ea751367a
DominikFrackowiak <rorschach@op.pl>
2024-06-11 22:28:33 +0200

    header done, refactoring to be done

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,7 +2,8 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import { useRouter, usePathname } from 'next/navigation'
+import { motion } from 'framer-motion'
 
 import { Roboto } from 'next/font/google'
 
@@ -16,17 +17,20 @@ import styles from './Header.module.scss'
 
 export default function Header({ searchParams }) {
 	const router = useRouter()
+	const pathname = usePathname()
+
+	console.log(pathname)
 	return (
-		<div className='responsiveWrapper'>
+		<header className='responsiveWrapper'>
 			<div
 				style={{
-					display: 'flex',
 					justifyContent: 'space-between',
 					alignItems: 'center',
 					width: '100%',
 					// border: '1px solid red',
-					marginBottom: '20px'
+					marginBottom: '20px',
 				}}
+				className={styles.headerDesktop}
 			>
 				<Link href='/'>
 					<Image
@@ -36,10 +40,12 @@ export default function Header({ searchParams }) {
 						alt='Marta Sieczkowska logo'
 					/>
 				</Link>
+
 				<nav className={styles.navigationMenu}>
 					{/* <Link href={'/?category=graphic-design'}>graphic design</Link>
 					<Link href={'/?category=illustration'}>illustration</Link>
 					<Link href='/#about'>about</Link> */}
+
 					<p
 						className={roboto.className}
 						onClick={() => {
@@ -92,6 +98,109 @@ export default function Header({ searchParams }) {
 					</p>
 				</nav>
 			</div>
-		</div>
+			<div className={styles.mobileHeader}>
+				{searchParams.menu === 'true' && (
+					<motion.div
+						initial={{ opacity: 0, y: '100%', zIndex: 0 }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0, y: '100%', zIndex: 1 }}
+						transition={{ delay: 0.5, duration: 0.7 }}
+					>
+						<nav className={styles.mobileNavigationMenu}>
+							<p
+								className={roboto.className}
+								onClick={() => {
+									if (searchParams.category !== 'graphic-design') {
+										router.push('/?category=graphic-design')
+									} else {
+										router.push('/')
+									}
+									router.refresh()
+								}}
+								style={{
+									fontWeight:
+										searchParams.category !== 'graphic-design'
+											? 'normal'
+											: 'bold',
+								}}
+							>
+								graphic design
+							</p>
+							<p
+								className={roboto.className}
+								onClick={() => {
+									if (searchParams.category !== 'illustration') {
+										router.push('/?category=illustration')
+									} else {
+										router.push('/')
+									}
+									router.refresh()
+								}}
+								style={{
+									fontWeight:
+										searchParams.category !== 'illustration'
+											? 'normal'
+											: 'bold',
+								}}
+							>
+								illustration
+							</p>
+							<p
+								className={roboto.className}
+								onClick={() => {
+									if (searchParams.about !== 'true') {
+										router.push('/?about=true')
+									} else {
+										router.push('/')
+									}
+									router.refresh()
+								}}
+								style={{
+									fontWeight: searchParams.about !== 'true' ? 'normal' : 'bold',
+								}}
+							>
+								about
+							</p>
+						</nav>
+					</motion.div>
+				)}
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'space-around',
+						alignItems: 'center',
+						width: '100%',
+						backgroundColor: '#fff',
+						zIndex: '400',
+					}}
+				>
+					<p
+						className={roboto.className}
+						onClick={() => {
+							if (searchParams.menu !== 'true') {
+								router.push('/?menu=true')
+							} else {
+								router.push('/')
+							}
+							router.refresh()
+						}}
+						style={{ padding: '15px 0', letterSpacing: '3px', cursor: 'pointer' }}
+					>
+						MENU
+					</p>
+
+					<Link href='/'>
+						<Image
+							src={'/assets/logo_marta_big.svg'}
+							width={145}
+							height={95}
+							alt='Marta Sieczkowska logo'
+							className={styles.mobileLogo}
+						/>
+					</Link>
+				</div>
+			</div>
+		</header>
 	)
 }
