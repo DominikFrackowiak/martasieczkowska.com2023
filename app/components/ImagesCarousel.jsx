0056d218ef58d0658d4981cefc20a985280b8597
DominikFrackowiak <rorschach@op.pl>
2024-06-11 12:36:55 +0200

    carousel update

--- app/components/ImagesCarousel.jsx
+++ app/components/ImagesCarousel.jsx
@@ -1,6 +1,7 @@
 import { Carousel } from '@ark-ui/react'
+import Image from 'next/image'
 
-export const ImagesCarousel = ({ images, index, handleStateChange}) => {
+export const ImagesCarousel = ({ images, index, handleStateChange }) => {
 	console.log(images)
 	return (
 		<Carousel.Root
@@ -13,17 +14,16 @@ export const ImagesCarousel = ({ images, index, handleStateChange}) => {
 				position: 'absolute',
 				top: '50%',
 				left: '50%',
-				transform: 'translate(-50%, -50%',
-				backgroundColor: 'rgba(255, 255, 255,.9',
-				zIndex: '100000'
+				transform: 'translate(-50%, -50%)',
+				backgroundColor: 'rgba(255, 255, 255, .9',
+				zIndex: '100000',
 			}}
 			onClick={handleStateChange}
 		>
-	
 			<Carousel.Viewport>
 				<Carousel.ItemGroup>
 					<Carousel.Item key={index} index={index}>
-						<img src={`${images[index]}`} alt={`Slide ${index}`} />
+						<img src={`${images[index]}`} alt={`Slide ${index}`} min-width='200px'/>
 					</Carousel.Item>
 				</Carousel.ItemGroup>
 			</Carousel.Viewport>
