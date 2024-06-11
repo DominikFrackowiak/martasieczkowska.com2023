eee6f38e0f0640e4acba17add1ab400ab694c2da
DominikFrackowiak <rorschach@op.pl>
2024-06-11 13:39:26 +0200

    ImagesCarousel update: image size limited, image centered

--- app/components/ImagesCarousel.jsx
+++ app/components/ImagesCarousel.jsx
@@ -3,7 +3,12 @@ import Image from 'next/image'
 
 import styles from './ImagesCarousel.module.scss'
 
-export const ImagesCarousel = ({ images, index, handleStateChange, imagesSizes }) => {
+export const ImagesCarousel = ({
+	images,
+	index,
+	handleStateChange,
+	imagesSizes,
+}) => {
 	console.log(images)
 	return (
 		<Carousel.Root
@@ -13,28 +18,33 @@ export const ImagesCarousel = ({ images, index, handleStateChange, imagesSizes }
 			spacing='16px'
 			orientation='horizontal'
 			style={{
-				position: 'absolute',
-				top: '50%',
-				left: '50%',
-				transform: 'translate(-50%, -50%)',
-				backgroundColor: 'rgba(255, 255, 255, .9',
+				// position: 'absolute',
+				// top: '50%',
+				// left: '50%',
+				// transform: 'translate(-50%, -50%)',
+				// backgroundColor: 'rgba(255, 255, 255, .9',
 				zIndex: '100000',
+
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
 			}}
+			className='responsiveWrapper'
 			onClick={handleStateChange}
 		>
 			<Carousel.Viewport>
 				<Carousel.ItemGroup>
 					<Carousel.Item key={index} index={index}>
-						<div className={styles.imageWrapper}>
+						{/* <div className={styles.imageWrapper}> */}
 						<Image
 							src={images[index]}
 							alt={`Slide ${index}`}
-							width={imagesSizes[index].width} 
-							height={imagesSizes[index].height} 
-							quality={90} 
+							width={imagesSizes[index].width}
+							height={imagesSizes[index].height}
+							quality={90}
 							className={styles.fullImage}
 						/>
-						</div>
+						{/* </div> */}
 					</Carousel.Item>
 				</Carousel.ItemGroup>
 			</Carousel.Viewport>
