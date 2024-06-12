65d108deabf3c82d17900c191760794470261be0
DominikFrackowiak <rorschach@op.pl>
2024-06-12 08:38:18 +0200

    suspense addeed in header due to the use of usesearchparams

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Suspense } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter, usePathname, useSearchParams } from 'next/navigation'
@@ -26,186 +27,185 @@ export default function Header() {
 
 	// console.log(pathname)
 	return (
-		<header className='responsiveWrapper'>
-			<div
-				style={{
-					justifyContent: 'space-between',
-					alignItems: 'center',
-					width: '100%',
-					// border: '1px solid red',
-					marginBottom: '20px',
-				}}
-				className={styles.headerDesktop}
-			>
-				<Link href='/'>
-					<Image
-						src={'/assets/logo_marta_big.svg'}
-						width={145}
-						height={95}
-						alt='Marta Sieczkowska logo'
-					/>
-				</Link>
-
-				<nav className={styles.navigationMenu}>
-					{/* <Link href={'/?category=graphic-design'}>graphic design</Link>
-					<Link href={'/?category=illustration'}>illustration</Link>
-					<Link href='/#about'>about</Link> */}
-
-					<p
-						className={roboto.className}
-						onClick={() => {
-							if (category !== 'graphic-design') {
-								router.push('/?category=graphic-design')
-							} else {
-								router.push('/')
-							}
-							router.refresh()
-						}}
-						style={{
-							fontWeight:
-								category !== 'graphic-design' ? 'normal' : 'bold',
-						}}
-					>
-						graphic design
-					</p>
-					<p
-						className={roboto.className}
-						onClick={() => {
-							if (category !== 'illustration') {
-								router.push('/?category=illustration')
-							} else {
-								router.push('/')
-							}
-							router.refresh()
-						}}
-						style={{
-							fontWeight:
-								category !== 'illustration' ? 'normal' : 'bold',
-						}}
-					>
-						illustration
-					</p>
-					<p
-						className={roboto.className}
-						onClick={() => {
-							if (about !== 'true') {
-								router.push('/?about=true')
-							} else {
-								router.push('/')
-							}
-							router.refresh()
-						}}
-						style={{
-							fontWeight: about !== 'true' ? 'normal' : 'bold',
-						}}
-					>
-						about
-					</p>
-				</nav>
-			</div>
-			<div className={styles.mobileHeader}>
-				{searchParams?.menu === 'true' && (
-					<motion.div
-						initial={{ opacity: 0, y: '100%', zIndex: 0 }}
-						animate={{ opacity: 1, y: 0 }}
-						exit={{ opacity: 0, y: '100%', zIndex: 1 }}
-						transition={{ delay: 0.5, duration: 0.7 }}
-					>
-						<nav className={styles.mobileNavigationMenu}>
-							<p
-								className={roboto.className}
-								onClick={() => {
-									if (category !== 'graphic-design') {
-										router.push('/?category=graphic-design')
-									} else {
-										router.push('/')
-									}
-									router.refresh()
-								}}
-								style={{
-									fontWeight:
-										category !== 'graphic-design'
-											? 'normal'
-											: 'bold',
-								}}
-							>
-								graphic design
-							</p>
-							<p
-								className={roboto.className}
-								onClick={() => {
-									if (category !== 'illustration') {
-										router.push('/?category=illustration')
-									} else {
-										router.push('/')
-									}
-									router.refresh()
-								}}
-								style={{
-									fontWeight:
-										category !== 'illustration'
-											? 'normal'
-											: 'bold',
-								}}
-							>
-								illustration
-							</p>
-							<p
-								className={roboto.className}
-								onClick={() => {
-									if (about !== 'true') {
-										router.push('/?about=true')
-									} else {
-										router.push('/')
-									}
-									router.refresh()
-								}}
-								style={{
-									fontWeight: about !== 'true' ? 'normal' : 'bold',
-								}}
-							>
-								about
-							</p>
-						</nav>
-					</motion.div>
-				)}
+		<Suspense fallback={<div>Loading...</div>}>
+			<header className='responsiveWrapper'>
 				<div
 					style={{
-						display: 'flex',
-						flexDirection: 'column',
-						justifyContent: 'space-around',
+						justifyContent: 'space-between',
 						alignItems: 'center',
 						width: '100%',
-						backgroundColor: '#fff',
-						zIndex: '400',
+						// border: '1px solid red',
+						marginBottom: '20px',
 					}}
+					className={styles.headerDesktop}
 				>
-					<p
-						className={roboto.className}
-						onClick={() => {
-							if (searchParams.menu !== 'true') {
-								router.push('/?menu=true')
-							} else {
-								router.push('/')
-							}
-							router.refresh()
-						}}
-						style={{ padding: '15px 0', letterSpacing: '3px', cursor: 'pointer' }}
-					>
-						MENU
-					</p>
-
 					<Link href='/'>
 						<Image
 							src={'/assets/logo_marta_big.svg'}
 							width={145}
 							height={95}
 							alt='Marta Sieczkowska logo'
-							className={styles.mobileLogo}
 						/>
 					</Link>
+
+					<nav className={styles.navigationMenu}>
+						{/* <Link href={'/?category=graphic-design'}>graphic design</Link>
+					<Link href={'/?category=illustration'}>illustration</Link>
+					<Link href='/#about'>about</Link> */}
+
+						<p
+							className={roboto.className}
+							onClick={() => {
+								if (category !== 'graphic-design') {
+									router.push('/?category=graphic-design')
+								} else {
+									router.push('/')
+								}
+								router.refresh()
+							}}
+							style={{
+								fontWeight: category !== 'graphic-design' ? 'normal' : 'bold',
+							}}
+						>
+							graphic design
+						</p>
+						<p
+							className={roboto.className}
+							onClick={() => {
+								if (category !== 'illustration') {
+									router.push('/?category=illustration')
+								} else {
+									router.push('/')
+								}
+								router.refresh()
+							}}
+							style={{
+								fontWeight: category !== 'illustration' ? 'normal' : 'bold',
+							}}
+						>
+							illustration
+						</p>
+						<p
+							className={roboto.className}
+							onClick={() => {
+								if (about !== 'true') {
+									router.push('/?about=true')
+								} else {
+									router.push('/')
+								}
+								router.refresh()
+							}}
+							style={{
+								fontWeight: about !== 'true' ? 'normal' : 'bold',
+							}}
+						>
+							about
+						</p>
+					</nav>
 				</div>
-			</div>
-		</header>
+				<div className={styles.mobileHeader}>
+					{searchParams?.menu === 'true' && (
+						<motion.div
+							initial={{ opacity: 0, y: '100%', zIndex: 0 }}
+							animate={{ opacity: 1, y: 0 }}
+							exit={{ opacity: 0, y: '100%', zIndex: 1 }}
+							transition={{ delay: 0.5, duration: 0.7 }}
+						>
+							<nav className={styles.mobileNavigationMenu}>
+								<p
+									className={roboto.className}
+									onClick={() => {
+										if (category !== 'graphic-design') {
+											router.push('/?category=graphic-design')
+										} else {
+											router.push('/')
+										}
+										router.refresh()
+									}}
+									style={{
+										fontWeight:
+											category !== 'graphic-design' ? 'normal' : 'bold',
+									}}
+								>
+									graphic design
+								</p>
+								<p
+									className={roboto.className}
+									onClick={() => {
+										if (category !== 'illustration') {
+											router.push('/?category=illustration')
+										} else {
+											router.push('/')
+										}
+										router.refresh()
+									}}
+									style={{
+										fontWeight: category !== 'illustration' ? 'normal' : 'bold',
+									}}
+								>
+									illustration
+								</p>
+								<p
+									className={roboto.className}
+									onClick={() => {
+										if (about !== 'true') {
+											router.push('/?about=true')
+										} else {
+											router.push('/')
+										}
+										router.refresh()
+									}}
+									style={{
+										fontWeight: about !== 'true' ? 'normal' : 'bold',
+									}}
+								>
+									about
+								</p>
+							</nav>
+						</motion.div>
+					)}
+					<div
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							justifyContent: 'space-around',
+							alignItems: 'center',
+							width: '100%',
+							backgroundColor: '#fff',
+							zIndex: '400',
+						}}
+					>
+						<p
+							className={roboto.className}
+							onClick={() => {
+								if (searchParams.menu !== 'true') {
+									router.push('/?menu=true')
+								} else {
+									router.push('/')
+								}
+								router.refresh()
+							}}
+							style={{
+								padding: '15px 0',
+								letterSpacing: '3px',
+								cursor: 'pointer',
+							}}
+						>
+							MENU
+						</p>
+
+						<Link href='/'>
+							<Image
+								src={'/assets/logo_marta_big.svg'}
+								width={145}
+								height={95}
+								alt='Marta Sieczkowska logo'
+								className={styles.mobileLogo}
+							/>
+						</Link>
+					</div>
+				</div>
+			</header>
+		</Suspense>
 	)
 }
--- app/not-found.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function NotFound() {
-  return (
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Not found</div>
-  )
-}
