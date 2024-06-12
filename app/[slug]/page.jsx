acfcd8d6257e9e5fbac855523bb7085c8cc07577
DominikFrackowiak <rorschach@op.pl>
2024-06-12 09:09:43 +0200

    update

--- app/[slug]/page.jsx
+++ app/[slug]/page.jsx
@@ -50,6 +50,7 @@ export default async function SinglePage({ params, searchParams }) {
 
 	if (searchParams.category !== undefined) {
 		data = await getSlugsByCategory(searchParams.category) 
+	} else {
 		data = await getAllSlugs()
 	}
 
